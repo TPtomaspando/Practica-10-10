c62d4fb4763288daacad0b6b0b62cc5970e469e5
Tomas Pando <tomaspando11@gmail.com>
2024-10-09 20:11:39 -0300

    realizamos un ejercicio en el que pasamos un texto de minusculas a mayusculas

---
+++ js/mayusculas.js
@@ -0,0 +1,5 @@
+// 13- Realiza un script que pida un texto y lo muestre en mayúsculas.
+
+let texto = prompt("Ingresa un texto:");
+let textoMayusculas = texto.toUpperCase();
+document.write("Texto en mayúsculas: " + textoMayusculas);
